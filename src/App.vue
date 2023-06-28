52980596b9e57e75c20279a88860595ef51288f1
HugoBzn <hugobazan1499@gmail.com>
2023-06-28 13:59:00 -0600

    Set header to better practices

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import Formulario from "./components/Formulario.vue";
 </script>
 
 <template>
-  <h1 class="titulo">Buscador de Clima</h1>
+  <header>
+    <h1 class="titulo">Buscador de Clima</h1>
+  </header>
 
   <div class="contenedor buscador-clima">
     <Formulario />
